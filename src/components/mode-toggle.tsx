b9ce05cadaca381dfe91f1b38e32641d7c3d1917
Ali <ali.hussein.pre@gmail.com>
2025-10-31 17:13:37 +0100

    feat: update header

---
+++ src/components/mode-toggle.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { useTheme } from "@/components/theme-provider";
+import { Button } from "@/components/ui/button";
+
+
+export function ModeToggle() {
+  const { theme, setTheme } = useTheme();
+  return (
+    <Button
+      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+      size="icon"
+      variant="ghost"
+      aria-label="Theme"
+      type="button"
+      className="size-8"
+    >
+      <svg
+        viewBox="0 0 32 32"
+        width="24"
+        height="24"
+        fill="currentcolor"
+        // style="display: block;"
+        className="block"
+      >
+        <circle
+          cx="16"
+          cy="16"
+          r="14"
+          fill="none"
+          stroke="currentcolor"
+          strokeWidth="4"
+        ></circle>
+        <path
+          d="
+        M 16 0
+        A 16 16 0 0 0 16 32
+        z"
+        ></path>
+      </svg>
+    </Button>
+  );
+}
---
+++ src/constants/urls.ts
@@ -0,0 +1,4 @@
+export const urls = {
+  github: "https://github.com/Ali-Hussein-dev/deep-react",
+  twitter: "https://x.com/alibey_10",
+};
