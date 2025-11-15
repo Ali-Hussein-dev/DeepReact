f2ebbf1101075932e10a71f742f12f8f7ab765a8
Ali <ali.hussein.pre@gmail.com>
2025-11-15 13:39:05 +0100

    feat: add templates page

--- src/integrations/convex/provider.tsx
+++ src/integrations/convex/provider.tsx
@@ -1,31 +1,34 @@
 import { ConvexQueryClient } from "@convex-dev/react-query";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ConvexProvider } from "convex/react";
+import { ConvexProvider, ConvexReactClient } from "convex/react";
 
 const CONVEX_URL = (import.meta as any).env.VITE_CONVEX_URL;
 if (!CONVEX_URL) {
 	console.error("missing envar CONVEX_URL");
 }
-const convexQueryClient = new ConvexQueryClient(CONVEX_URL);
 
-const queryClient = new QueryClient({
-	defaultOptions: {
-		queries: {
-			queryKeyHashFn: convexQueryClient.hashFn(),
-			queryFn: convexQueryClient.queryFn(),
-		},
-	},
-});
-convexQueryClient.connect(queryClient);
 
 export default function AppConvexProvider({
 	children,
 }: {
 	children: React.ReactNode;
 }) {
-	return (
-		<ConvexProvider client={convexQueryClient.convexClient}>
-			<QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-		</ConvexProvider>
-	);
+	const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL);
+	const convexQueryClient = new ConvexQueryClient(convex);
+	const queryClient = new QueryClient({
+	  defaultOptions: {
+		queries: {
+		  queryKeyHashFn: convexQueryClient.hashFn(),
+		  queryFn: convexQueryClient.queryFn(),
+		},
+	  },
+	});
+	convexQueryClient.connect(queryClient);
+  return (
+    <ConvexProvider client={convexQueryClient.convexClient}>
+      <QueryClientProvider client={queryClient}>
+        {children}
+      </QueryClientProvider>
+    </ConvexProvider>
+  );
 }
