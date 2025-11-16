cc9b514cfcfc4f9505126d6af7405ea808f03a61
Ali <ali.hussein.pre@gmail.com>
2025-11-16 11:10:02 +0100

    add netlify plugin

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,15 @@ import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
 import { cloudflare } from '@cloudflare/vite-plugin'
-
+import netlify from '@netlify/vite-plugin-tanstack-start'
 const config = defineConfig({
+  // server: {
+  //   allowedHosts: ['combining-valves-bacon-related.trycloudflare.com'],
+  // },
   plugins: [
     devtools(),
     cloudflare({ viteEnvironment: { name: 'ssr' } }),
+    netlify(),
     // this is the plugin that enables path aliases
     viteTsConfigPaths({
       projects: ['./tsconfig.json'],
