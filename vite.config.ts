0589979369852a184ed413a53872af95e90b4926
Ali <ali.hussein.pre@gmail.com>
2025-11-26 09:13:49 +0100

    remove netlify

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
 import { cloudflare } from '@cloudflare/vite-plugin'
-import netlify from '@netlify/vite-plugin-tanstack-start'
+
 const config = defineConfig({
   // server: {
   //   allowedHosts: ['combining-valves-bacon-related.trycloudflare.com'],
@@ -13,7 +13,6 @@ const config = defineConfig({
   plugins: [
     devtools(),
     cloudflare({ viteEnvironment: { name: 'ssr' } }),
-    netlify(),
     // this is the plugin that enables path aliases
     viteTsConfigPaths({
       projects: ['./tsconfig.json'],
